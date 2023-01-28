06c13044b2a21b31a6d865295a9aac6bacef2d8d
Austin Fullwood <fullwood.austin@gmail.com>
2023-01-27 23:07:40 -0500

    Add backend

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "homepage": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "homepage4e8bbf1d": {
       "AppClientID": "string",
